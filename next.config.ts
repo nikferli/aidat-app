716c845bfb5c1e5d454e5f32ba190912d790fd8d
nikferli <701176@gmail.com@gmail.com>
2026-09-01 22:59:26 +0300

    next.config - sw headers duzeltildi

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
 
-async headers() {
-  return [{
-    source: '/sw.js',
-    headers: [{ key: 'Service-Worker-Allowed', value: '/' }]
-  }]
-}
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/sw.js',
+        headers: [{ key: 'Service-Worker-Allowed', value: '/' }]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
